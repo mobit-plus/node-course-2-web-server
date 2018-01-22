bba5ba3d7b8af00051a74a9d60db1538d2667a45
mobit <mobit@gmail.com>
2018-01-22 15:19:08 +0530

    Initial commit

---
+++ server.js
@@ -0,0 +1,64 @@
+const express = require('express');
+const hbs = require('hbs');
+const fs = require('fs');
+
+var app = express();
+
+
+hbs.registerPartials(__dirname + '/views/Pratials');
+
+hbs.registerHelper('getcurrentyear',() => {
+    return new Date().getFullYear()
+});
+
+hbs.registerHelper('scremit',(text) =>{
+   return text.toUpperCase();
+});
+
+app.set('view engine', 'hbs');
+
+
+app.use((req,res,next) =>{
+    var now = new Date().toString();
+    var log = `${now} ${req.method} ${req.url}`;
+    
+
+    fs.appendFileSync("server.log", log + '\n', (error) =>{
+        if(error){
+            console.log('Unable to server.log file append!');
+        }
+    });
+    console.log(log);
+    next();
+});
+
+app.use((req,res,next) =>{
+    res.render('maintenence.hbs');
+});
+
+app.use(express.static(__dirname + '/public'));
+
+app.get('/',(req,res) => {
+    // res.send('<h1>Hello Express!</h1>');
+    res.render('home.hbs', {
+        Title: 'Home Page!',
+        welcome: 'welcome my website!'
+        
+    });
+});
+
+
+app.get('/bad',(req,res) =>{
+    res.send({
+        errorMessage: 'Unable to Connect!',
+    })
+ });
+ app.get('/about',(req,res) =>{
+    res.render('about.hbs',{
+        Title: 'About Page',
+        
+    });
+ });
+app.listen(3000,() => {
+    console.log('The server connect to 3000 port');
+});
